d154092ba32ce4303c382da3708474694226882a
fuguoheng <fuguoheng@kuaishou.com>
2025-06-01 20:48:04 +0800

    Auto commit on 2025年 6月 1日 星期日 20时48分04秒 CST

--- hnswlib_self/hnswlib/hnswalg.h
+++ hnswlib_self/hnswlib/hnswalg.h
@@ -1113,18 +1113,14 @@ class HierarchicalNSW : public AlgorithmInterface<dist_t> {
         for (auto& node: merge_graph) {
             auto& external_neighbours = node.external_neighbours_;
             auto& internal_neighbours = node.internal_neighbours_;
-            std::cout << "node max level: " << node.max_level_ << " external_neighbours size: " << external_neighbours.size() 
-                << " internal_neighbours size: " << internal_neighbours.size() << std::endl;
 
             for (int level = 0; level <= node.max_level_; level++) {
-                std::cout << "external_neighbours level size: " << external_neighbours[level].size() << std::endl;
                 for (int i = 0; i < external_neighbours[level].size(); i++) {
                     auto it = external_label_to_internal_id.find(external_neighbours[level][i]);
                     if (it == external_label_to_internal_id.end()) {
                         std::cout << "error: external id not found" << std::endl;
                         throw std::runtime_error("external id not found");
                     } else {
-                        std::cout << "internal id: " << it->second << std::endl;
                         internal_neighbours[level].push_back(it->second);
                     }
                 }
