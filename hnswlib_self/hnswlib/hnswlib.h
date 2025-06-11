ee45b7a25d62c40192be48e3765efc34061d23a5
fgh <fgh@fghdeMacBook-Pro.local>
2025-06-11 16:01:45 +0800

    Auto commit on 2025年 6月11日 星期三 16时01分45秒 CST

--- hnswlib_self/hnswlib/hnswlib.h
+++ hnswlib_self/hnswlib/hnswlib.h
@@ -226,3 +226,5 @@ AlgorithmInterface<dist_t>::searchKnnCloserFirst(const void* query_data, size_t 
 #include "stop_condition.h"
 #include "bruteforce.h"
 #include "hnswalg.h"
+#include "space_pq.h"
+#include "space_int8.h"
